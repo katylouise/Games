fb4c517f856d5d28c962f1deadab460b35ac4e90
Rebecca Appleyard <rklappleyard@gmail.com>
2015-01-10 20:10:32 +0000

    added new features

--- Reaction Tester/reaction.js
+++ Reaction Tester/reaction.js
@@ -3,10 +3,12 @@
 var clickedTime;
 var createdTime;
 var reactionTime;
-
+var totalTime = 0;
+var count = 0;
+var fastestTime = 100;
 //create box after random time < 4 seconds
 function boxAppear() {
-	
+
 	setTimeout(function() {
 		createdTime = $.now();
 		$('#box').show();
@@ -29,11 +31,20 @@ function boxAppear() {
 	}
 	//randomly position shape
 	var top = Math.random() * 250;
-	var left = Math.random() * 800;
+	var left = Math.random() * 900;
 
 	$('#box').css({'left':left});
 	$('#box').css({'top':top});
 
+	//randomly change size
+	var size = Math.random() * 150;
+
+	if (size < 40) {
+		var size = Math.random() * 150;
+	}
+
+	$('#box').css({'width': size});
+	$('#box').css({'height': size});
 
 }
 //disappear box on clicking
@@ -41,17 +52,43 @@ function boxDisappear() {
 	$('#box').click(function() {
 		clickedTime = $.now();
 		reactionTime = (clickedTime - createdTime)/1000;
-		$('#time').html(reactionTime);
+		$('#time').html(reactionTime + " seconds");
+
+		totalTime += reactionTime;
+		
+		if (reactionTime < fastestTime)
+		{
+			fastestTime = reactionTime;
+		}
 
 		$(this).hide();
+		count += 1;
+
+		if (count < 10) {
 		boxAppear();
+		}
+		else {
+			gameOver();
+		}
 	});
 }
+
+function gameOver() {
+				$('#your_time').hide();
+
+				averageTime = totalTime / 10;
+				$('#average').html(averageTime);
+				$('#fastest').html(fastestTime);
+				$('#gameover').show();
+				count = 0;
+			}
 //start on click of button		
 function start() {
 $('button').click(function() {
 	$('#instructions').hide();
+	$('#gameover').hide();
 	$('#your_time').show();
+	$('#time').html('')
 	boxAppear();
 	});
 }
